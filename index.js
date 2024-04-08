1e4ed24c0da60078edfa8a5f8a5c76aaf1c98188
donewithwork <donewithworkyt@gmail.com>
2024-04-08 17:49:36 +0800

    coin market cap

--- index.js
+++ index.js
@@ -17,11 +17,45 @@ let trendingState = "";
  * @returns {Promise<number>} The price of Bitcoin in USD
  */
 const GetPrice = async () => {
-  const res = await axios({
-    method: "GET",
-    url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
-  });
-  return Number(res.data.bitcoin.usd * 10000);
+  const API_KEY = process.env.Coin_Market_Cap_Api;
+
+  // Base URL for the CoinMarketCap API
+  const BASE_URL =
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
+
+  // Symbol for Bitcoin
+  const symbol = "BTC";
+
+  // Parameters for the request
+  const params = {
+    symbol: symbol,
+    convert: "USD", // You can specify the currency you want to convert the price to
+  };
+
+  // Headers containing your API key
+  const headers = {
+    Accepts: "application/json",
+    "X-CMC_PRO_API_KEY": API_KEY,
+  };
+  let newPrice = 0;
+  // Making the request using Axios
+  await axios
+    .get(BASE_URL, { params, headers })
+    .then((response) => {
+      if (response.status === 200) {
+        const data = response.data;
+        // Extracting the price of Bitcoin
+        const bitcoinPrice = data.data[symbol].quote.USD.price;
+        console.log(`Current price of Bitcoin (BTC): $${bitcoinPrice}`);
+        newPrice = bitcoinPrice;
+      } else {
+        console.error("Error: Unable to fetch data from CoinMarketCap API");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  return newPrice;
 };
 
 /**
@@ -86,7 +120,7 @@ const tweet = async () => {
       Lift Off Moon GIF By Stakin
       `;
 
-    UpdateNewPrice(price);
+    await UpdateNewPrice(price);
     console.log(multilineText);
     const mediaId = await twitterClient.v1.uploadMedia("./gifs/giphy.gif");
     console.log(mediaId);
@@ -105,6 +139,7 @@ const tweet = async () => {
 app.use(express.json());
 app.get("/tweet", async (req, res) => {
   const status = await tweet();
+
   if (status === true) {
     res.send("Tweeted successfully!");
   } else {
